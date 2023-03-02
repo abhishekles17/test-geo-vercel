53d1479b8adf1ae9adb3c7432d6743042a83b9de
“abhishekles17” <abhiraj10069@gmail.com>
2023-03-02 11:56:26 +0530

    cookies idea3

--- middleware.js
+++ middleware.js
@@ -24,7 +24,7 @@ console.log(req.url)
 
 
    const res = NextResponse.rewrite(url)
-   res.cookies.set("location",`_${city}_${country}_${region}_2234`);
+   res.cookies.set("location",`_${city}_${country}_${region}_22324`);
    return res;
  
 }
