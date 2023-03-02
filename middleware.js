1d19d01aed0c234260327fa1bfcc7a9d6dd31c82
“abhishekles17” <abhiraj10069@gmail.com>
2023-03-02 11:51:42 +0530

    cookies idea3

--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export const config = {
-    matcher: '/bst-es',
+    matcher: ['/bst-es/'],
   }
 
 export async function middleware(req) {
